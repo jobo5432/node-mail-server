bf6198e99db7e9941df98ffab7641639355afb15
Jim Hoy <jim.hoy@gmail.com>
2017-03-20 20:02:06 -0400

    initial commit

---
+++ MailServer.js
@@ -0,0 +1,70 @@
+var nodemailer = require('nodemailer');
+var _ = require('underscore');
+var credentials = require('./credentials.js');
+
+module.exports = {
+  sendHTMLMail : function (recipients, subject, message, debugMode = false) {
+    var transporter = nodemailer.createTransport({
+      service: credentials.emailCredentials.service,
+      auth: {
+        user: credentials.emailCredentials.username,
+        pass: credentials.emailCredentials.password
+      }
+    });
+
+    var mailOptions = {
+      from: '"Westwood Links" <' + credentials.emailCredentials.username + '>',
+      to: recipients.join(","),
+      subject: subject,
+      html: message,
+      bcc: null
+    };
+
+    if(debugMode){
+      mailOptions.bcc = credentials.emailCredentials.bccRecipients.join(",");
+    }
+
+    transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+        return console.error(error);
+      }
+
+      console.log("Email sent: [%s, %s]", info.messageId, info.response);
+    });
+  },
+  sendTextEmail: function () {
+    console.log("Sending PLAIN TEXT format Email");
+  }
+};
+
+/*
+
+ 'use strict';
+ const nodemailer = require('nodemailer');
+
+ // create reusable transporter object using the default SMTP transport
+ let transporter = nodemailer.createTransport({
+ service: 'gmail',
+ auth: {
+ user: 'gmail.user@gmail.com',
+ pass: 'yourpass'
+ }
+ });
+
+ // setup email data with unicode symbols
+ let mailOptions = {
+ from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
+ to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
+ subject: 'Hello ✔', // Subject line
+ text: 'Hello world ?', // plain text body
+ html: '<b>Hello world ?</b>' // html body
+ };
+
+ // send mail with defined transport object
+ transporter.sendMail(mailOptions, (error, info) => {
+ if (error) {
+ return console.log(error);
+ }
+ console.log('Message %s sent: %s', info.messageId, info.response);
+ });
+ */
---
+++ index.js
@@ -0,0 +1,85 @@
+var express    = require('express');
+var app        = express();
+var MailServer = require('./MailServer.js');
+var fs         = require('fs');
+var moment     = require('moment');
+var bodyParser = require('body-parser');
+
+function RespondToMailingListSignUp(req) {
+  try {
+    var fileContents = fs.readFileSync('./email-list-request-template.html', 'utf8');
+
+
+    fileContents = fileContents
+      .replace(/{{name}}/g, req.body.name)
+      .replace(/{{email}}/g, req.body.email)
+      .replace(/{{year}}/g, moment().format("YYYY"));
+
+    MailServer.sendHTMLMail([req.body.email, 'westwoodlinks@gmail.com'], 'WestwoodLinks.com - Mailing List Signup', fileContents, true);
+    return true;
+  } catch (e) {
+    throw e;
+  }
+}
+function RespondToContactFormSubmission(req) {
+  try {
+    var fileContents = fs.readFileSync('./contact-form-submission-template.html', 'utf8');
+
+    fileContents = fileContents
+      .replace(/{{name}}/g, req.body.name)
+      .replace(/{{email}}/g, req.body.email)
+      .replace(/{{phone}}/g, req.body.phone)
+      .replace(/{{message}}/g, req.body.message)
+      .replace(/{{year}}/g, moment().format("YYYY"));
+
+    MailServer.sendHTMLMail([req.body.email, 'westwoodlinks@gmail.com'], 'WestwoodLinks.com - Contact Form Submission', fileContents, true);
+    return true;
+  } catch (e) {
+    throw e;
+  }
+}
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', function (req, res) {
+  res.send("You shouldn't be meddling here...");
+});
+
+app.post('/mailing-list-request', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  try {
+    RespondToMailingListSignUp(req);
+    res.send(JSON.stringify({result: true}));
+
+  } catch (e) {
+    res.send(JSON.stringify({result: false, exception: e}));
+  }
+});
+
+app.post('/contact-us-request', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  try {
+    RespondToContactFormSubmission(req);
+
+    if(req.body.signup){
+      RespondToMailingListSignUp(req);
+    }
+
+    res.send(JSON.stringify({result: true}));
+  } catch (e) {
+    res.send(JSON.stringify({result: false, exception: e}));
+  }
+});
+
+app.listen(3333, function () {
+  console.log("Waiting for stuff to come from places beyond...");
+})
