cb93ae3b79a78eb72fa7c3a55dbb413965e362a4
Jim Hoy <jim.hoy@gmail.com>
2017-03-21 15:38:56 -0400

    changed CORS headers to allow site to send email properly

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function RespondToContactFormSubmission(req) {
 }
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "http://localhost:80");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
