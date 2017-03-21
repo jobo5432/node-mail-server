eefe155ce38db98c5356f6fe80759114c10de52c
Jim Hoy <jim.hoy@gmail.com>
2017-03-20 20:05:26 -0400

    adding censored credentials.js

---
+++ credentials.js
@@ -0,0 +1,8 @@
+module.exports = {
+  emailCredentials: {
+    service : 'gmail',
+    username: 'you@gmail.com',
+    password: '2fa password',
+    bccRecipients: ['people@iwanttobecopiedonthis']
+  }
+};
