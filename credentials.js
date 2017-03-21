7ffe6f7b2e3fcfd87af4df49eb70c8cd1fb66c4c
Jim Hoy <jim.hoy@gmail.com>
2017-03-20 20:18:10 -0400

    rebuilding bad stuff

--- credentials.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  emailCredentials: {
-    service : 'gmail',
-    username: 'you@gmail.com',
-    password: '2fa password',
-    bccRecipients: ['people@iwanttobecopiedonthis']
-  }
-};
